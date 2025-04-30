94fe5425c7372387d645f33b5acb3e1c4c703c95
Jack4271 <yfchan4271@gmail.com>
2025-04-30 14:16:06 +0100

    render3

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   );
 } else {
-  
+
   app.get('/', (req, res) => {
     res.send('API is running....');
   });
